3de9370b4a501be4322ff75ebce7faf2862984d2
Ilyas-koro02 <301505110224lost>
2020-07-04 03:37:18 +0600

    add line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import java.util.Spliterator;
+
 public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello World!!!");
+	    System.out.println("lol");
     }
 }
